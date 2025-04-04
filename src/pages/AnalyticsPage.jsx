bfd1c1334dcf71a36c60ca1f6eeeb06f08759981
Vera Mstislavtseva <mstislavtsevavv@gmail.com>
2025-04-04 17:00:47 +0300

    feat(AnalyticsPage): add ChannelPerfomance component

--- src/pages/AnalyticsPage.jsx
+++ src/pages/AnalyticsPage.jsx
@@ -1,6 +1,7 @@
 import Header from "../components/common/Header";
 import AnalyticOverviewCards from "../components/analytics/AnalyticOverviewCards";
 import RevenueChart from "../components/analytics/RevenueChart";
+import ChannelPerfomance from "../components/analytics/ChannelPerfomance";
 
 const AnalyticsPage = () => {
   return (
@@ -9,6 +10,9 @@ const AnalyticsPage = () => {
       <main className="max-w-7xl mx-auto py-6 px-4 lg:px-8">
         <AnalyticOverviewCards />
         <RevenueChart />
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 mb-8 ">
+          <ChannelPerfomance />
+        </div>
       </main>
     </div>
   );
