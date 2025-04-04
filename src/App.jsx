1030c512066ca4f8e27b8358d825677f5dabc04e
Vera Mstislavtseva <mstislavtsevavv@gmail.com>
2025-04-04 20:15:45 +0300

    feat: create base layout for Settings page

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import UsersPage from "./pages/UsersPage";
 import SalesPage from "./pages/SalesPage";
 import OrdersPage from "./pages/OrdersPage.jsx";
 import AnalyticsPage from "./pages/AnalyticsPage.jsx";
+import SettingsPage from "./pages/SettingsPage.jsx";
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
         <Route path="/sales" element={<SalesPage />} />
         <Route path="/orders" element={<OrdersPage />} />
         <Route path="/analytics" element={<AnalyticsPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
       </Routes>
     </div>
   );
